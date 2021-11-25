a0b61a87a79b15d423f0cc8e29ca66e514b6186f
dafatov <dafatov@yandex.ru>
2021-11-25 19:41:44 +0300

    - fix status bg size

--- src/utils/attachments.js
+++ src/utils/attachments.js
@@ -9,8 +9,11 @@ module.exports.createStatus = async (queue) => {
     const canvas = createCanvas(510, 40);
     const context = canvas.getContext("2d");
 
+    let remainedTmp = `-${timeFormatmSeconds(remained(queue))}`;
+    context.font = '24px sans-serif';
+
     context.fillStyle = '#2F3136';
-    context.fillRect(0, 0, 45 + 107, 40);
+    context.fillRect(0, 0, 49 + context.measureText(remainedTmp).width, 40);
 
     context.fillStyle = config.colors.info;
     context.fillRect(0, 0, 5, 40);
@@ -18,9 +21,8 @@ module.exports.createStatus = async (queue) => {
     const status = await loadImage(`./res/icons/${getStatusIcon(queue.nowPlaying)}.png`);
     context.drawImage(status, 9, 4, 32, 32);
 
-    context.font = '24px sans-serif';
     context.fillStyle = config.colors.info;
-    context.fillText(`-${timeFormatmSeconds(remained(queue))}`, 45, 28);
+    context.fillText(remainedTmp, 45, 28);
 
     const attachment = new MessageAttachment(canvas.toBuffer(), 'status.png');
 
