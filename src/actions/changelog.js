a2b67ef66fd6c1ba745668171390d01e6626c410
dafatov <dafatov@yandex.ru>
2022-05-28 00:26:59 +0300

    Hotfix Исправлена обработка не измененного changelogDoc.js

--- src/actions/changelog.js
+++ src/actions/changelog.js
@@ -1,7 +1,7 @@
 const {getLast, add} = require("../repositories/changelog");
 const changelogDoc = require("../configs/changelogDoc");
 const config = require("../configs/config");
-const {log} = require("../utils/logger");
+const {log, error} = require("../utils/logger");
 
 module.exports.init = async () => {
   if (!changelogDoc.isPublic || ((await getLast())?.version ?? 0) >= config.version) {
@@ -11,6 +11,6 @@ module.exports.init = async () => {
     await add(config.version, changelogDoc.text)
     log('Успешно зарегистрирована история изменений');
   } else {
-    throw 'История изменений не обновлена!!!'
+    error('История изменений не обновлена!!!');
   }
 }
