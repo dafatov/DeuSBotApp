0a2f6058f55c945864fdeef6041284b14e0cff52
dafatov <dafatov@yandex.ru>
2022-08-30 20:44:42 +0300

    Clear files from old deployments

--- src/configs/changelog.js
+++ src/configs/changelog.js
@@ -2,7 +2,7 @@
  * @type {{isPublic: boolean, message: {features: string[], bugfixes: string[], ad: string | null, footer: string | null, announce: string | null}}}
  */
 module.exports = {
-  isPublic: true,
+  isPublic: false,
   message: {
     ad: 'В случае нахождения недостатков или предложений используйте команду /issue. Чем подробнее Вы опишите свою заявку, тем быстрее она будет рассмотрена',
     announce: 'Теперь у бота есть система доступа. А значит, что по умолчанию никто не имеет никаких прав... хех.. Мне впадлу, поэтому пусть Вова (<@233923369685352449>) этим занимается',
