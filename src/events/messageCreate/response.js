81528e76b3c1c26cdda346570a0aee8d9b6c413f
dafatov <dafatov@yandex.ru>
2023-02-02 19:14:08 +0300

    - Рефакторинг с правкой ошибок

--- src/events/messageCreate/response.js
+++ src/events/messageCreate/response.js
@@ -1,38 +1,34 @@
 const {CATEGORIES, TYPES} = require('../../db/repositories/audit');
 const {audit} = require('../../actions/auditor');
-const db = require('../../db/repositories/responses');
+const {getAll} = require('../../db/repositories/responses');
 const {stringify} = require('../../utils/string');
 const {t} = require('i18next');
 
-module.exports.execute = async ({message}) => {
+module.exports.execute = ({message}) => {
   if (message.author.bot) {
     return;
   }
 
-  try {
-    await Promise.all(db.getAll(message.guildId).then(rules => rules.map(async e => {
-      if (!e.regex || !e.react) {
-        throw t('inner:error.response', {regex: e.regex, react: e.react});
-      }
+  return getAll(message.guildId)
+    .then(rules => rules
+      .map(async e => {
+        if (!e.regex || !e.react) {
+          throw t('inner:error.response', {regex: e.regex, react: e.react});
+        }
 
-      if (message.content.match(e.regex)) {
-        await message.reply(`${e.react}`);
-        await audit({
-          guildId: message.guild.id,
-          type: TYPES.INFO,
-          category: CATEGORIES.RESPONSE,
-          message: t('inner:audit.response', {message: message.content, regex: e.regex, react: e.react}),
-        });
-      }
-    })).catch(e => {
-      throw e;
-    }));
-  } catch (e) {
-    await audit({
+        if (message.content.match(e.regex)) {
+          await message.reply(`${e.react}`);
+          await audit({
+            guildId: message.guild.id,
+            type: TYPES.INFO,
+            category: CATEGORIES.RESPONSE,
+            message: t('inner:audit.response', {message: message.content, regex: e.regex, react: e.react}),
+          });
+        }
+      })).catch(e => audit({
       guildId: null,
       type: TYPES.ERROR,
       category: CATEGORIES.RESPONSE,
       message: stringify(e),
-    });
-  }
+    }));
 };
