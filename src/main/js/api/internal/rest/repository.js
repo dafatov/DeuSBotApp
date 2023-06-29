b7547c36c99c94b379ccc10a3593405ef0923b3c
dafatov <dafatov@yandex.ru>
2023-06-29 21:27:07 +0300

    Добавил получение audit'а по id через rest

--- src/main/js/api/internal/rest/repository.js
+++ src/main/js/api/internal/rest/repository.js
@@ -1,6 +1,7 @@
 const {SCOPES} = require('../../../db/repositories/permission');
 const {authCheckForbidden} = require('../security');
 const fs = require('fs');
+const {getAll} = require('../../../db/repositories/audit');
 const {stringify} = require('../../../utils/string');
 
 module.exports = {
@@ -18,6 +19,13 @@ module.exports = {
             [repository]: require(`../../../db/repositories/${repository}`).clearCache?.() ?? null,
           }), {}))
         .then(repositories => res.status(200).json(repositories))
+        .catch(error => res.status(403).send(stringify(error))));
+
+    app.get('/repository/audit?:id', (req, res) =>
+      authCheckForbidden(req.headers.authorization?.split(' ')[1], SCOPES.API_AUDITOR_AUDIT)
+        .then(() => getAll())
+        .then(audit => audit.find(entry => entry.id === req.query.id) ?? {})
+        .then(entry => res.status(200).json(entry))
         .catch(error => res.status(403).send(stringify(error))));
   },
 };
