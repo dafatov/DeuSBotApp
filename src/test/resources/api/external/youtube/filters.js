f8eb3817ecd0efcf25d2c4f69b0d5a19ec6bbdd8
dafatov <dafatov@yandex.ru>
2023-07-19 20:20:59 +0300

    Правки ревью

--- src/test/resources/api/external/youtube/filters.js
+++ src/test/resources/api/external/youtube/filters.js
@@ -1,268 +1,244 @@
-module.exports = new Map(
+module.exports = new Map([
   [
-    [
-      'Uploaddate',
-      new Map([
-        [
-          'Lasthour',
-          {
-            name: 'Lasthour',
-            active: false,
-            url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIIAQ%253D%253D',
-            description: 'SearchforLasthour',
-          },
-        ], [
-          'Today',
-          {
-            name: 'Today',
-            active: false,
-            url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIIAg%253D%253D',
-            description: 'SearchforToday',
-          },
-        ], [
-          'Thisweek', {
-            name: 'Thisweek',
-            active: false,
-            url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIIAw%253D%253D',
-            description: 'SearchforThisweek',
-          },
-        ], [
-          'Thismonth', {
-            name: 'Thismonth',
-            active: false,
-            url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIIBA%253D%253D',
-            description: 'SearchforThismonth',
-          },
-        ], [
-          'Thisyear', {
-            name: 'Thisyear',
-            active: false,
-            url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIIBQ%253D%253D',
-            description: 'SearchforThisyear',
-          },
-        ],
-      ]),
-    ],
-    [
-      'Type',
-      new Map(
-        [
-          [
-            'Video',
-            {
-              name: 'Video',
-              active: false,
-              url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIQAQ%253D%253D',
-              description: 'SearchforVideo',
-            },
-          ],
-          [
-            'Channel',
-            {
-              name: 'Channel',
-              active: false,
-              url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIQAg%253D%253D',
-              description: 'SearchforChannel',
-            },
-          ],
-          [
-            'Playlist',
-            {
-              name: 'Playlist',
-              active: false,
-              url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIQAw%253D%253D',
-              description: 'SearchforPlaylist',
-            },
-          ],
-          [
-            'Movie',
-            {
-              name: 'Movie',
-              active: false,
-              url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIQBA%253D%253D',
-              description: 'SearchforMovie',
-            },
-          ],
-        ]),
-    ],
-    [
-      'Duration',
-      new Map(
-        [
-          [
-            'Under4minutes',
-            {
-              name: 'Under4minutes',
-              active: false,
-              url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIYAQ%253D%253D',
-              description: 'SearchforUnder4minutes',
-            },
-          ],
-          [
-            '4-20minutes',
-            {
-              name: '4-20minutes',
-              active: false,
-              url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIYAw%253D%253D',
-              description: 'Searchfor4-20minutes',
-            },
-          ],
-          [
-            'Over20minutes',
-            {
-              name: 'Over20minutes',
-              active: false,
-              url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIYAg%253D%253D',
-              description: 'SearchforOver20minutes',
-            },
-          ],
-        ]),
-    ],
-    [
-      'Features',
-      new Map(
-        [
-          [
-            'Live',
-            {
-              name: 'Live',
-              active: false,
-              url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgJAAQ%253D%253D',
-              description: 'SearchforLive',
-            },
-          ],
-          [
-            '4K',
-            {
-              name: '4K',
-              active: false,
-              url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgJwAQ%253D%253D',
-              description: 'Searchfor4K',
-            },
-          ],
-          [
-            'HD',
-            {
-              name: 'HD',
-              active: false,
-              url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIgAQ%253D%253D',
-              description: 'SearchforHD',
-            },
-          ],
-          [
-            'Subtitles/CC',
-            {
-              name: 'Subtitles/CC',
-              active: false,
-              url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIoAQ%253D%253D',
-              description: 'SearchforSubtitles/CC',
-            },
-          ],
-          [
-            'CreativeCommons',
-            {
-              name: 'CreativeCommons',
-              active: false,
-              url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIwAQ%253D%253D',
-              description: 'SearchforCreativeCommons',
-            },
-          ],
-          [
-            '360°',
-            {
-              name: '360°',
-              active: false,
-              url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgJ4AQ%253D%253D',
-              description: 'Searchfor360°',
-            },
-          ],
-          [
-            'VR180',
-            {
-              name: 'VR180',
-              active: false,
-              url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgPQAQE%253D',
-              description: 'SearchforVR180',
-            },
-          ],
-          [
-            '3D',
-            {
-              name: '3D',
-              active: false,
-              url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgI4AQ%253D%253D',
-              description: 'Searchfor3D',
-            },
-          ],
-          [
-            'HDR',
-            {
-              name: 'HDR',
-              active: false,
-              url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgPIAQE%253D',
-              description: 'SearchforHDR',
-            },
-          ],
-          [
-            'Location',
-            {
-              name: 'Location',
-              active: false,
-              url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgO4AQE%253D',
-              description: 'SearchforLocation',
-            },
-          ],
-          [
-            'Purchased',
-            {
-              name: 'Purchased',
-              active: false,
-              url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgJIAQ%253D%253D',
-              description: 'SearchforPurchased',
-            },
-          ],
-        ]),
-    ],
-    [
-      'Sortby',
-      new Map(
-        [
-          [
-            'Relevance',
-            {
-              name: 'Relevance',
-              active: true,
-              url: null,
-              description: 'Sortbyrelevance',
-            },
-          ],
-          [
-            'Uploaddate',
-            {
-              name: 'Uploaddate',
-              active: false,
-              url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=CAI%253D',
-              description: 'Sortbyuploaddate',
-            },
-          ],
-          [
-            'Viewcount',
-            {
-              name: 'Viewcount',
-              active: false,
-              url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=CAM%253D',
-              description: 'Sortbyviewcount',
-            },
-          ],
-          [
-            'Rating',
-            {
-              name: 'Rating',
-              active: false,
-              url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=CAE%253D',
-              description: 'Sortbyrating',
-            },
-          ],
-        ]),
-    ],
-  ]);
+    'Uploaddate',
+    new Map([
+      [
+        'Lasthour',
+        {
+          name: 'Lasthour',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIIAQ%253D%253D',
+          description: 'SearchforLasthour',
+        },
+      ], [
+        'Today',
+        {
+          name: 'Today',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIIAg%253D%253D',
+          description: 'SearchforToday',
+        },
+      ], [
+        'Thisweek',
+        {
+          name: 'Thisweek',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIIAw%253D%253D',
+          description: 'SearchforThisweek',
+        },
+      ], [
+        'Thismonth',
+        {
+          name: 'Thismonth',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIIBA%253D%253D',
+          description: 'SearchforThismonth',
+        },
+      ], [
+        'Thisyear',
+        {
+          name: 'Thisyear',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIIBQ%253D%253D',
+          description: 'SearchforThisyear',
+        },
+      ],
+    ]),
+  ], [
+    'Type',
+    new Map([
+      [
+        'Video',
+        {
+          name: 'Video',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIQAQ%253D%253D',
+          description: 'SearchforVideo',
+        },
+      ], [
+        'Channel',
+        {
+          name: 'Channel',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIQAg%253D%253D',
+          description: 'SearchforChannel',
+        },
+      ], [
+        'Playlist',
+        {
+          name: 'Playlist',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIQAw%253D%253D',
+          description: 'SearchforPlaylist',
+        },
+      ], [
+        'Movie',
+        {
+          name: 'Movie',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIQBA%253D%253D',
+          description: 'SearchforMovie',
+        },
+      ],
+    ]),
+  ], [
+    'Duration',
+    new Map([
+      [
+        'Under4minutes',
+        {
+          name: 'Under4minutes',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIYAQ%253D%253D',
+          description: 'SearchforUnder4minutes',
+        },
+      ], [
+        '4-20minutes',
+        {
+          name: '4-20minutes',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIYAw%253D%253D',
+          description: 'Searchfor4-20minutes',
+        },
+      ], [
+        'Over20minutes',
+        {
+          name: 'Over20minutes',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIYAg%253D%253D',
+          description: 'SearchforOver20minutes',
+        },
+      ],
+    ]),
+  ], [
+    'Features',
+    new Map([
+      [
+        'Live',
+        {
+          name: 'Live',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgJAAQ%253D%253D',
+          description: 'SearchforLive',
+        },
+      ], [
+        '4K',
+        {
+          name: '4K',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgJwAQ%253D%253D',
+          description: 'Searchfor4K',
+        },
+      ], [
+        'HD',
+        {
+          name: 'HD',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIgAQ%253D%253D',
+          description: 'SearchforHD',
+        },
+      ], [
+        'Subtitles/CC',
+        {
+          name: 'Subtitles/CC',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIoAQ%253D%253D',
+          description: 'SearchforSubtitles/CC',
+        },
+      ], [
+        'CreativeCommons',
+        {
+          name: 'CreativeCommons',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgIwAQ%253D%253D',
+          description: 'SearchforCreativeCommons',
+        },
+      ], [
+        '360°',
+        {
+          name: '360°',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgJ4AQ%253D%253D',
+          description: 'Searchfor360°',
+        },
+      ], [
+        'VR180',
+        {
+          name: 'VR180',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgPQAQE%253D',
+          description: 'SearchforVR180',
+        },
+      ], [
+        '3D',
+        {
+          name: '3D',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgI4AQ%253D%253D',
+          description: 'Searchfor3D',
+        },
+      ], [
+        'HDR',
+        {
+          name: 'HDR',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgPIAQE%253D',
+          description: 'SearchforHDR',
+        },
+      ], [
+        'Location',
+        {
+          name: 'Location',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgO4AQE%253D',
+          description: 'SearchforLocation',
+        },
+      ], [
+        'Purchased',
+        {
+          name: 'Purchased',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=EgJIAQ%253D%253D',
+          description: 'SearchforPurchased',
+        },
+      ],
+    ]),
+  ], [
+    'Sortby',
+    new Map([
+      [
+        'Relevance',
+        {
+          name: 'Relevance',
+          active: true,
+          url: null,
+          description: 'Sortbyrelevance',
+        },
+      ], [
+        'Uploaddate',
+        {
+          name: 'Uploaddate',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=CAI%253D',
+          description: 'Sortbyuploaddate',
+        },
+      ], [
+        'Viewcount',
+        {
+          name: 'Viewcount',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=CAM%253D',
+          description: 'Sortbyviewcount',
+        },
+      ], [
+        'Rating',
+        {
+          name: 'Rating',
+          active: false,
+          url: 'https://www.youtube.com/results?search_query=red+alert+3+song&sp=CAE%253D',
+          description: 'Sortbyrating',
+        },
+      ],
+    ]),
+  ],
+]);
