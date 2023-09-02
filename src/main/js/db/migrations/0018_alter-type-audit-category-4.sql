2ae6372dc1ff812c34d39681fad23d8cefbee9fd
dafatov <33876950+dafatov@users.noreply.github.com>
2023-09-02 18:44:54 +0300

    Fix error: "cannot run ALTER TYPE ... ADD inside a transaction block" (#82)

--- src/main/js/db/migrations/0018_alter-type-audit-category-4.sql
+++ src/main/js/db/migrations/0018_alter-type-audit-category-4.sql
@@ -1,1 +1,25 @@
-alter type audit_category add value 'radio';
+ALTER TABLE audit
+    ALTER COLUMN category TYPE varchar;
+
+DROP TYPE IF EXISTS audit_category;
+
+CREATE TYPE audit_category AS enum (
+    'player',
+    'uncategorized',
+    'init',
+    'auditor',
+    'database',
+    'api',
+    'permission',
+    'command',
+    'state_update',
+    'security',
+    'response',
+    'publicist',
+    'listener',
+    'locale',
+    'radio'
+    );
+
+ALTER TABLE audit
+    ALTER COLUMN category TYPE audit_category USING (category::audit_category)
--- src/main/js/db/migrations/0033_alter-type-song-type.sql
+++ src/main/js/db/migrations/0033_alter-type-song-type.sql
@@ -1,1 +1,13 @@
-ALTER TYPE song_type ADD VALUE 'file';
+ALTER TABLE queue
+    ALTER COLUMN type TYPE varchar;
+
+DROP TYPE IF EXISTS song_type;
+
+CREATE TYPE song_type AS enum (
+    'youtube',
+    'radio',
+    'file'
+    );
+
+ALTER TABLE queue
+    ALTER COLUMN type TYPE song_type USING (type::song_type)
